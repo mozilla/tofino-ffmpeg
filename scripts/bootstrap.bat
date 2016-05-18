ff39da1f2e780c9e8c69430c59426edaff12d859
Dave Townsend <dtownsend@oxymoronical.com>
2016-05-18 09:01:54 -0700

    Fix windows packaging.

--- scripts/bootstrap.bat
+++ scripts/bootstrap.bat
@@ -5,3 +5,4 @@ mkdir -p yasm
 curl -fsSL -o yasm\yasm.exe http://www.tortall.net/projects/yasm/releases/yasm-1.3.0-win64.exe
 
 set target_arch=%architecture:x86=ia32%
+set /p REVISION=<LCC_REVISION
--- scripts/package.bat
+++ scripts/package.bat
@@ -1,3 +1,5 @@
 mkdir -p dist
 
-cp ffmpeg/%target_arch%/*.dll dist
+copy ffmpeg\\%target_arch%\\*.dll dist
+cd dist
+7z a ..\ffmpeg-win32-%target_arch%.zip *
